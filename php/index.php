5cbb3738cb98e421f6f6911dc82614e76dd78e9b
wenhui.yu <winfreyu@gmail.com>
2019-10-18 15:36:10 +0800

    Update php clients

--- php/index.php
+++ php/index.php
@@ -1,22 +1,22 @@
 <?php
 use  mdanter\ecc;
-$API_KEY = "x";
-$API_SECRET = "x";
-$HOST = "https://api.sandbox.cobo.com";
+$APP_KEY = "x";
+$APP_SECRET = "x";
+$HOST = "http://razzil-api.dev.csiodev.com";
 
 require __DIR__ . "/vendor/autoload.php";
 
 use Elliptic\EC;
 
 function sign_hmac($message){
-    global $API_SECRET;
-    return hash_hmac("sha256", $message, $API_SECRET);
+    global $APP_SECRET;
+    return hash_hmac("sha256", $message, $APP_SECRET);
 }
 function sign_ecdsa($message){
-    global $API_SECRET;
+    global $APP_SECRET;
     $message = hash("sha256", hash("sha256", $message, True), True);
     $ec = new EC('secp256k1');
-    $key = $ec->keyFromPrivate($API_SECRET);
+    $key = $ec->keyFromPrivate($APP_SECRET);
     return $key->sign(bin2hex($message))->toDER('hex');
 }
 function sort_data($data){
@@ -29,14 +29,14 @@ function sort_data($data){
 }
 function request($method, $path, $data){
     global $HOST;
-    global $API_KEY;
+    global $APP_KEY;
     $ch = curl_init();
     $sorted_data = sort_data($data);
     $nonce = time() * 1000;
     curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 10);
     curl_setopt ($ch, CURLOPT_HTTPHEADER, [
-        "Biz-Api-Key:".$API_KEY,
+        "Biz-Api-Key:".$APP_KEY,
         "Biz-Api-Nonce:".$nonce,
         "Biz-Api-Signature:".sign_ecdsa(join("|", [$method, $path, $nonce, $sorted_data]))
     ]);
@@ -52,6 +52,6 @@ function request($method, $path, $data){
     return $file_contents;
 }
 
-echo request("GET", "/v1/custody/coin_info/", ["coin" => "BTC"]);
-echo request("POST", "/v1/custody/new_address/", ["coin" => "BTC"]);
+echo request("GET", "/v1/payment/".$paymentId);
+echo request("POST", "/v1/payment/", ["amount" => 1024, "expiry" => 1800]);
 
